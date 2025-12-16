9eca82fc095e2f9d06b2f2d8c36df0d5533e14ef
Roger BC <roger@chaindots.com>
2025-12-16 13:59:16 -0300

    feat(client): agregar componente ErrorMessage para mostrar mensajes de error

---
+++ client/src/components/ErrorMessage.jsx
@@ -0,0 +1,5 @@
+export const ErrorMessage = ({ message = 'Something went wrong' }) => (
+  <div className="bg-red-900 border border-red-700 text-red-300 px-4 py-3 rounded-lg">
+    {message}
+  </div>
+);
