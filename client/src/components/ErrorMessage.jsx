9580769c5e982c9ade515be47700ddda01f60efa
Roger BC <roger@chaindots.com>
2025-12-16 14:21:00 -0300

    fix(ErrorMessage): corregir la estructura del mensaje de error para mejorar la legibilidad

--- client/src/components/ErrorMessage.jsx
+++ client/src/components/ErrorMessage.jsx
@@ -1,5 +1,5 @@
 export const ErrorMessage = ({ message = 'Something went wrong' }) => (
   <div className="bg-red-900 border border-red-700 text-red-300 px-4 py-3 rounded-lg">
-    {message}
+    <p className="text-red-300 text-sm text-center">{message}</p>
   </div>
 );
