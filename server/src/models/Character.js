02d411df4cfbac099799b3c517ef19fe6b94ea52
Roger BC <roger@chaindots.com>
2026-01-16 13:57:05 -0300

    feat(server): agregar descripción al esquema de personaje

--- server/src/models/Character.js
+++ server/src/models/Character.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 const characterSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
+    description: { type: String, default: '' },
     playerId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'User',
